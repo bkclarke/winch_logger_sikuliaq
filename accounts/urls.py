da4c13f1d66a346b37a9579959175b6b439b9606
Bonny Clarke <bonny.k.clarke@gmail.com>
2024-04-03 23:42:36 +0000

    troubleshoot logout url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,4 @@ from . import views
 
 urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(), {'template_name': 'registration/login.html'}, name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), {'template_name': 'registration/logged_out.html'}, name = 'logout')
 ]
